7491277cca8b44f42a3f00c3a900b713702bbf16
Ángela Carmona <angelacarmona325@gmail.com>
2025-05-13 19:21:41 +0200

    model.js modificado (sigue habiendo el mismo error)

--- DeliverUS-Backend/src/models/models.js
+++ DeliverUS-Backend/src/models/models.js
@@ -6,6 +6,7 @@ import loadProductModel from './Product.js'
 import loadProductCategoryModel from './ProductCategory.js'
 import loadRestaurantCategoryModel from './RestaurantCategory.js'
 import loadUserModel from './User.js'
+import loadReviewModel from './Review.js'
 
 const sequelizeSession = new Sequelize(getEnvironmentConfig().database, getEnvironmentConfig().username, getEnvironmentConfig().password, getEnvironmentConfig())
 const Restaurant = loadRestaurantModel(sequelizeSession, Sequelize.DataTypes)
@@ -14,6 +15,7 @@ const Product = loadProductModel(sequelizeSession, Sequelize.DataTypes)
 const ProductCategory = loadProductCategoryModel(sequelizeSession, Sequelize.DataTypes)
 const RestaurantCategory = loadRestaurantCategoryModel(sequelizeSession, Sequelize.DataTypes)
 const User = loadUserModel(sequelizeSession, Sequelize.DataTypes)
+const Review = loadReviewModel(sequelizeSession, Sequelize.DataTypes)
 
 const db = { Restaurant, Order, Product, ProductCategory, RestaurantCategory, User }
 
@@ -23,4 +25,4 @@ Object.keys(db).forEach(modelName => {
   }
 })
 
-export { Restaurant, Order, Product, ProductCategory, RestaurantCategory, User, sequelizeSession }
+export { Restaurant, Order, Product, ProductCategory, RestaurantCategory, User, Review, sequelizeSession }
